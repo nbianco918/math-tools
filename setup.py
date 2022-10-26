4011e2336110d80ef6d97bc66b838806994eed9b
nbianco918 <116698515+nbianco918@users.noreply.github.com>
2022-10-25 20:59:54 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='math-tools',
+      version='0.0.1',
+      description='Compilation of ODE Solvers and Math Functions',
+      author='Nathan Bianco',
+      packages=['mathtools'])
