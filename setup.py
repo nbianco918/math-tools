4a0178d02d0631738d0eee8946aed21148f917e1
nbianco918 <116698515+nbianco918@users.noreply.github.com>
2022-10-25 21:25:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='math-tools',
+setup(name='math-tools-nbianco0918',
       version='0.0.1',
       description='Compilation of ODE Solvers and Math Functions',
       author='Nathan Bianco',
